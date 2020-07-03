dd543a1e7f85ee96df540d65c11cead0f74123bf
Yevgenyi Kotvytskyi <yegeniy.kotvytskyi@gmail.com>
2020-07-03 19:52:38 +0300

    Start lesson 22_2

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 /* jshint -W014 */
 /* global alert, confirm, console, prompt, console */
 /* jshint esversion: 6 */
-
+'use strict';
 
 
 class Todo {
@@ -23,6 +23,7 @@ class Todo {
         e.preventDefault();
         const input = this.input.value.trim();
         if (input) {
+            this.input.value = '';
             const newTodo = {
                 value: input,
                 completed: false,
@@ -49,6 +50,7 @@ class Todo {
         li.insertAdjacentHTML('beforeend', `
             <span class="text-todo">${todo.value}</span>
             <div class="todo-buttons">
+                <button class="todo-edit"></button>
                 <button class="todo-remove"></button>
                 <button class="todo-complete"></button>
             </div>
@@ -58,27 +60,85 @@ class Todo {
         } else {
             this.todoList.append(li);
         }
-
     }
+
+    dropTodo(element, dalay, background) {
+        element.style.zIndex = 100;
+        element.style.transitionDuration = dalay + 's';
+        element.style.backgroundColor = background;
+        const container = element.closest('.todo');
+        let offset = 0;
+        if (container.matches('.todo-list')) offset = container.offsetTop;
+        element.style.transform = `translateY(${(offset + container.clientHeight - element.offsetTop)}px)`;
+    }
+
+
+    moveTodo(element, dalay, background) {
+        const
+            container = element.closest('.todo'),
+            containerCompleted = document.getElementById('completed'),
+            oldBackground = element.style.backgroundColor;
+        let offset = 0;
+
+        element.style.backgroundColor = background;
+
+        if (container.matches('.todo-list')) {
+            offset = container.offsetTop + containerCompleted.clientHeight +
+                 container.clientHeight - element.offsetTop;
+        } else {
+            offset = -element.offsetTop;
+        }
+
+        element.style.backgroundColor = oldBackground;
+        element.style.transform = `translateY(${offset}px)`;
+        element.style.zIndex = 100;
+        element.style.transitionDuration = dalay + 's';
+    }
+
 
     generateKey() {
         return Math.random().toString(36).substr(2, 15);
     }
 
     delitedItem(target) {
-        const key = target.closest('li').dataset.key;
+        const li = target.closest('li'),
+            key = li.dataset.key;
         if (key) {
-            this.todoData.delete(key);
-            this.render();
+            const delay = 2;
+            this.dropTodo(li, delay, 'grey');
+            setTimeout(() => {
+                this.todoData.delete(key);
+                this.render();
+            }, delay * 1000);
         }
     }
 
     completedItem(target) {
-        const key = target.closest('li').dataset.key;
+        const li = target.closest('li'),
+            key = li.dataset.key;
+        if (key) {
+            const delay = 2;
+            this.moveTodo(li, delay, 'yellow');
+            setTimeout(() => {
+                const todo = this.todoData.get(key);
+                todo.completed = !todo.completed;
+                this.render();
+            }, delay * 1000);
+        }
+    }
+
+    editItem(target) {
+        const li = target.closest('li'),
+            key = li.dataset.key;
         if (key) {
             const todo = this.todoData.get(key);
-            todo.completed = !todo.completed;
-            this.render();
+            const newValue = prompt(`Редактирование дела ${todo.value}:`, todo.value);
+            if (newValue.trim()) {
+                todo.value = newValue.trim();
+                this.render();
+            } else {
+                alert('Невозможно записать пустое значение.\n Редактирование отменено!');
+            }
         }
     }
 
@@ -88,6 +148,8 @@ class Todo {
             this.delitedItem(target);
         } else if (target.matches('button.todo-complete')) {
             this.completedItem(target);
+        } else if (target.matches('button.todo-edit')) {
+            this.editItem(target);
         }
     }
 
@@ -105,3 +167,5 @@ class Todo {
 const todo = new Todo('.todo-control', '.header-input', '.todo-list', '.todo-completed');
 
 todo.init();
+
+
